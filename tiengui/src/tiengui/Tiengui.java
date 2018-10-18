f2d0d142eb530c18d2823318be374abac682f8f1
NgoQuocDat <shaco14091999@gmail.com>
2018-10-18 13:59:35 +0700

    add

---
+++ tiengui/src/tiengui/Tiengui.java
@@ -0,0 +1,29 @@
+package tiengui;
+
+import java.util.Scanner;
+
+public class Tiengui {
+
+    public static void main(String[] args) {
+        double money=1.0, interset_rate=1.0;
+        int month;
+        Scanner sc = new Scanner(System.in);
+        
+         System.out.println("Enter investment amount: ");
+        money = sc.nextDouble();
+        
+         System.out.println("Enter number of months: ");
+        month = sc.nextInt();
+        
+         System.out.println("Enter annual interest rate in percentage: ");
+        interset_rate = sc.nextDouble();
+        
+         double total_interset = money;
+         for(int i = 0; i < month; i++){
+             total_interset += total_interset * ((double)interset_rate/100)/12;
+         }
+         
+         System.out.println("Total of interset: " + total_interset); 
+    }
+    
+}
